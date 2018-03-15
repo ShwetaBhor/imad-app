eafe3245fe9914b162f46834bb21ba68ba0efc1d
ShwetaBhor <36539089+ShwetaBhor@users.noreply.github.com>
2018-03-15 15:36:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-1', function (req, res) {
+  res.sendFile('article-one request will get serve here!!');
+});
+
+app.get('/article-2', function (req, res) {
+  res.sendFile('article-two request will get serve here!!');
+});
+
+app.get('/article-3', function (req, res) {
+  res.sendFile('article-three request will get serve here!!');
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
