2765c4d521a3f33f5ccc56b3c6b04da8b59f8b2c
ShwetaBhor <36539089+ShwetaBhor@users.noreply.github.com>
2018-03-15 17:33:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -26,39 +26,37 @@ function createTemplate(Data) {
     var heading=data.heading;
     var date=data.date;
     var content=data.content;
-
-
-var htmlTamplate = `
-<html>
-    <head>
-        <title>
-           ${title}
-            </title>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />  
-          <link href="/ui/style.css" rel="stylesheet" />
-        
-        </head>
-    <body>
-        <div class ="container">
-        <div>
-            <a href="/">home</a>
-            </div>
-        <hr/> 
-        <h3>
-            ${heading}
-        </h3>
-        
-        <div>
-          ${date}
-        </div>
-         <div>
-           ${content}
-        </div>
-        </div>
-        </body>    
-        
-        </html>
-`;
+    var htmlTamplate = `
+          <html>
+            <head>
+                <title>
+                   ${title}
+                    </title>
+                <meta name="viewport" content="width=device-width,initial-scale=1" />  
+                  <link href="/ui/style.css" rel="stylesheet" />
+                
+                </head>
+            <body>
+                <div class ="container">
+                <div>
+                    <a href="/">home</a>
+                    </div>
+                <hr/> 
+                <h3>
+                    ${heading}
+                </h3>
+                
+                <div>
+                  ${date}
+                </div>
+                 <div>
+                   ${content}
+                </div>
+                </div>
+                </body>    
+                
+                </html>`
+                ;
     return htmlTamplate;
 }
 
