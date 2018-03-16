12882831a127e136ae4fb8e85b292245cc958509
ShwetaBhor <36539089+ShwetaBhor@users.noreply.github.com>
2018-03-16 20:26:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,6 @@
-console.log('Loaded!');
-var element=document.getElementById('main-text');
-
-element.innerHTML="Shweta Bhor";
-
-
-
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft= marginLeft+ 'px';
-}
-img.onClick= function(){
-    var interval=setInterval(moveRight,100);
+var button = document.getElementById('counter');
+button.onClick= function(){
+    counter =counter+1;
+    var span= document.getElementById('count');
+    span.innerHTML=counter.toString();
 };
